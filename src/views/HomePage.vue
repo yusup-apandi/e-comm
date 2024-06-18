0239f88518da59fb6b32d9b5ca3a208811d1c0c9
Yusup Apandi <yusup18apandi@gmail.com>
2024-06-18 20:45:53 +0700

    push

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from '../components/Navbar.vue';
 import Footer from '../components/Footer.vue';
+import Slider from '../components/Slider.vue';
 </script>
 
 <template>
     <Navbar />
-       <div>
+       <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <Slider />
         <h1>ini sample text</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, nobis id voluptatem odio voluptatum alias dolores velit, nemo, quo porro est ullam neque corporis error iste tenetur adipisci ad non.</p>
     </div>
