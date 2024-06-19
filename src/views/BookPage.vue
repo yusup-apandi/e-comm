c87c47d112ae9c41470ddec4dede9f097e01f7f8
Yusup Apandi <yusup18apandi@gmail.com>
2024-06-19 13:18:19 +0700

    update link

--- src/views/BookPage.vue
+++ src/views/BookPage.vue
@@ -175,7 +175,7 @@
         <h2 id="products-heading" class="sr-only">Buku</h2>
 
         <div class="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          <a v-for="book in books" :key="book.id" href="/detailbuku" class="group">
+          <a v-for="book in books" :key="book.id" href="./bookdetail" class="group">
             <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg sm:aspect-w-2 sm:aspect-h-3">
               <img :src="book.imageSrc" :alt="book.imageAlt" class="h-full w-full object-cover object-center group-hover:opacity-75" />
             </div>
